167ff74f96ea29b10f0614523b1775e73618e4f5
Bradley McLeish <bmcleish@podfather.com>
2022-11-09 16:09:17 +0000

    import from src file

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -15,9 +15,8 @@
 
 <script>
 import StarterKit from '@tiptap/starter-kit'
-import Mention from '@tiptap/extension-mention'
 import Placeholder from '@tiptap/extension-placeholder'
-// import { Mention } from '../extension-mergetag/mergeTag.ts'
+import Mention from '../extension-mergetag'
 import { Editor, EditorContent } from '@tiptap/vue-3'
 import tags from '../tags'
 
